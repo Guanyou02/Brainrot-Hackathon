e13cc6598c779b0c9e1de39caf6c482bb4a5f8a2
Guanyou02 <guanyou.edit@gmail.com>
2024-11-30 20:27:49 +0800

    Change the navigation bar page to bold when you are on that page and also added more stories to databse.

--- server/src/configs/initTables.js
+++ server/src/configs/initTables.js
@@ -11,7 +11,10 @@ CREATE TABLE Stories (
 
 INSERT INTO Stories (user_id, name, moment) VALUES
 (1, 'Timothy', 'When she yells at you for forgetting what she told you earlier today, but you are just a chill guy enjoying your day'),
-(2, 'Bob', 'When my life is in shambles but then I remember Im just a chill guy who understands that sometimes shit just happens');
+(2, 'Bob', 'When my life is in shambles but then I remember Im just a chill guy who understands that sometimes shit just happens'),
+(3, 'James', 'When I send bro a video late night about the "Chill Guy" so he responds cuz we both are chill guys'),
+(4, 'Alex', 'When your boss is mad at you for coming in late all the time but you're just a chill guy that oversleeps sometimes'),
+(5, 'Daniel', 'When you parents ask "Why do you shower for so long?" but you are just a chill guy who enjoys the warm water');
 `;
 
 pool.query(SQLSTATEMENT, (error, results, fields) => {
